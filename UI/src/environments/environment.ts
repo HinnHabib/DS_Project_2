220b80854b8b10e14df6b9375b1b08701ce4a1ae
HinnHabib <habibissahinn@gmail.com>
2018-04-28 22:46:57 +0300

    Add UI Project Infrastructure

--- UI/src/environments/environment.ts
+++ UI/src/environments/environment.ts
@@ -4,5 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  URL: 'https://localhost:8080/',
+  PATH_URL: ''
 };
