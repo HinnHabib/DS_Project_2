a9966df9d74c08575b7b99f1161e8cc6d4fbcebf
Hussein-Dahir <hussein_dahir@hotmail.com>
2018-05-05 22:26:35 +0300

    some changes

--- Supermarket/app/src/main/java/com/example/fadi/supermarket/fragment/LoginFragment.java
+++ Supermarket/app/src/main/java/com/example/fadi/supermarket/fragment/LoginFragment.java
@@ -1,10 +1,7 @@
 package com.example.fadi.supermarket.fragment;
 
-
 import android.app.Fragment;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,14 +14,7 @@ import com.example.fadi.supermarket.R;
 import com.example.fadi.supermarket.activity.HomeActivity;
 import com.example.fadi.supermarket.async.task.AsyncResponse;
 import com.example.fadi.supermarket.async.task.SignInAsyncTaskRunner;
-import com.example.fadi.supermarket.util.HttpManager;
 
-import static android.content.Context.MODE_PRIVATE;
-
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class LoginFragment extends Fragment implements AsyncResponse {
 
 
@@ -39,9 +29,8 @@ public class LoginFragment extends Fragment implements AsyncResponse {
         View myView = inflater.inflate(R.layout.fragment_login, container, false);
 
         Button loginButton = (Button) myView.findViewById(R.id.finalLoginButton);
-        final EditText emailET = (EditText) myView.findViewById(R.id.nameLogin);
+        final EditText emailET = (EditText) myView.findViewById(R.id.emailLogin);
         final EditText passwordET = (EditText) myView.findViewById(R.id.passwordLogin);
-        final CheckBox rememberMeCheck = (CheckBox) myView.findViewById(R.id.rememberLogin);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,13 +40,6 @@ public class LoginFragment extends Fragment implements AsyncResponse {
 
                 SignInAsyncTaskRunner signInAsyncTaskRunner = new SignInAsyncTaskRunner(LoginFragment.this);
                 signInAsyncTaskRunner.execute(email, password);
-
-//                if (rememberMeCheck.isChecked()) {
-//                    SharedPreferences userDetails = getContext().getSharedPreferences("userdetails", MODE_PRIVATE);
-//                    userDetails.putString("email", email);
-//                    userDetails.putString("password", password);
-//                }
-
             }
         });
 
--- Supermarket/app/src/main/java/com/example/fadi/supermarket/util/HttpManager.java
+++ Supermarket/app/src/main/java/com/example/fadi/supermarket/util/HttpManager.java
@@ -6,15 +6,11 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
-
-import javax.net.ssl.HttpsURLConnection;
 
 public class HttpManager {
 
